b0d98450153b0c1a4f16fc64c6415f0b5d03a9b0
Anurag Yadav <anurag.yadav@live.in>
2021-08-15 22:52:51 +0530

    15 AUg 10:52 PM

--- src/test/java/DumpData/TestBrowser.java
+++ src/test/java/DumpData/TestBrowser.java
@@ -2,18 +2,32 @@ package DumpData;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import pages.HomePage;
 
 public class TestBrowser {
 	@Test
 	public void BrowserTest() {
-		String pathTOExecutable="D:\\Education\\Selenium Workspace\\geckodriver.exe";
 		
-		System.setProperty("webdriver.gecko.driver", pathTOExecutable);
-		WebDriver driver=new FirefoxDriver();
-		driver.get("https://google.com");
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.phptravels.net/");
 		driver.manage().window().maximize();
+		HomePage.NavigateToBlog(driver);
+		HomePage.NavigateToFlights(driver);
+		HomePage.NavigateToHome(driver);
+		HomePage.NavigateToHotels(driver);
+		HomePage.NavigateToOffers(driver);
+		HomePage.NavigateToTours(driver);
+		HomePage.NavigateToVisa(driver);
+		HomePage.NavigateToHome(driver);
+		HomePage.SelectFlightsTab(driver);
+		HomePage.SelectToursTab(driver);
+		HomePage.SelectVisaTab(driver);
+		HomePage.SelectHotelsTab(driver);
 	}
 
 }
--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -17,8 +17,40 @@ public class HomePage {
 		return driver.findElement(By.linkText("Signup"));
 	}
 	
+	public static void NavigateToHome(WebDriver driver) {
+		driver.findElement(By.linkText("Home")).click();
+	}
 	
+	public static void NavigateToHotels(WebDriver driver) {
+		driver.findElement(By.linkText("Hotels")).click();
+	}
+	public static void NavigateToFlights(WebDriver driver) {
+		driver.findElement(By.xpath("//a[text()='flights' and @title='home']")).click();
+	}
+	public static void NavigateToTours(WebDriver driver) {
+		driver.findElement(By.linkText("Tours")).click();
+	}
+	public static void NavigateToVisa(WebDriver driver) {
+		driver.findElement(By.xpath("//a[text()='visa' and @title='home']")).click();
+	}
+	public static void NavigateToBlog(WebDriver driver) {
+		driver.findElement(By.linkText("Blog")).click();
+	}
+	public static void NavigateToOffers(WebDriver driver) {
+		driver.findElement(By.linkText("Offers")).click();
+	}
 	
-
+	public static void SelectHotelsTab(WebDriver driver) {
+		driver.findElement(By.xpath("//button[@id='hotels-tab' and @data-bs-target='#hotels']")).click();
+	}
+	public static void SelectFlightsTab(WebDriver driver) {
+		driver.findElement(By.xpath("//button[@id='hotels-tab' and @data-bs-target='#flights']")).click();
+	}
+	public static void SelectToursTab(WebDriver driver) {
+		driver.findElement(By.id("tours-tab")).click();
+	}
+	public static void SelectVisaTab(WebDriver driver) {
+		driver.findElement(By.id("visa-tab")).click();
+	}
 
 }
