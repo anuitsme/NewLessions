ec6e6afec07d78bd27914d1694a570e8bc336054
Anurag Yadav <anuragyadav05@live.in>
2021-08-16 17:01:14 +0530

    Stale Elemnt Excpetion

--- src/test/java/DumpData/TestBrowser.java
+++ src/test/java/DumpData/TestBrowser.java
@@ -1,5 +1,7 @@
 package DumpData;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -17,6 +19,7 @@ public class TestBrowser {
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.phptravels.net/");
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		HomePage_HotelsTab.selectCityFromDropdown(driver, "Delhi");
 	}
 
--- src/test/java/pages/HomePage_HotelsTab.java
+++ src/test/java/pages/HomePage_HotelsTab.java
@@ -1,9 +1,10 @@
 package pages;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -12,7 +13,7 @@ public class HomePage_HotelsTab {
 	
 	public static WebElement CityInput(WebDriver driver) {
 		
-		
+		driver.findElement(By.xpath("//span[@id='select2-hotels_city-container']")).click();
 		return driver.findElement(By.xpath("//input[@class='select2-search__field']"));
 	}
 	
@@ -21,11 +22,33 @@ public class HomePage_HotelsTab {
 			String searchString=cityName.substring(0,3);
 			
 			CityInput(driver).sendKeys(searchString);
-			
+			try {
+				Thread.currentThread();
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			List<WebElement> results=driver.findElements(By.xpath("//ul[@id='select2-hotels_city-results']//li"));
-			
+			//System.out.println(results.size());
 			for(WebElement el: results) {
-				System.out.println(el.getAttribute("innerText"));
+				//System.out.println(el.getAttribute("innerText"));
+				//String attr=el.getAttribute("innerText");
+				try {
+					if((el.getAttribute("innerText")).contains(cityName)) {
+						try {
+							el.click();
+						} catch (StaleElementReferenceException e) {
+							String xpath="//ul[@id='select2-hotels_city-results']//li[text()='"+el.getAttribute("innerText")+"']";
+							System.out.println(xpath);
+							driver.findElement(By.xpath(xpath)).click();
+							
+						}
+					}
+				} catch (StaleElementReferenceException e) {
+					// TODO Auto-generated catch block
+					//e.printStackTrace();
+				}
 				
 			}
 		
